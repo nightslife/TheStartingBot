8ba0aee0435defb65ccad9605ea06828477d2b98
Sean Seltzer <sseltzer431@gmail.com>
2017-05-21 04:14:04 -0400

    Added the ability to forward DMs to the bot to a particular user.

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const client = new Discord.Client();
 
 
 const clientBotId = '';
+const guildId = '107926187082285056';
 const applicationChannelId = '315720532794540052';
 const officerChannelId = '315720554353262593';
 const officerRoleId = '<@315732496635527170>';
+const officerForForwardingId = '102380839358189568';
 const applicationMessagedeleteTime = 3000;
 
+let officerForForwarding;
 let officerChannel;
 
-client.on('ready', () => {
-  console.log('I am ready!');
-  officerChannel = client.channels.get(officerChannelId);
-});
-
-client.on('message', message => {
-  if (!message.channel || !message.channel.name || message.channel.id !== applicationChannelId) return;
-
+function pasteApplicationToOfficerChannel(message) {
   // Get the current user.
   let user = message.member.user;
 
@@ -34,10 +30,41 @@ client.on('message', message => {
   // DM the applicant and let them know their message was received.
   user.createDM().then((channel) => {
     channel.send(`Thank you for your submission ${user.username}! An officer will be with you as soon as possible.`);
+  }).catch((err) => {
+    console.log(err);
   });
 
   // Delete the message after a set time.
   message.delete(applicationMessagedeleteTime);
+}
+
+function relayDMsToOfficer(message) {
+  // Do not forward messages that the bot is sending out
+  if (client.user.id === message.author.id) return;
+  let user = message.author;
+  // Forward the message to the officer in charge
+  officerForForwarding.createDM().then((channel) => {
+    // Copy their message message into the DM using a rich text format for easy reading.
+    const embed = new Discord.RichEmbed()
+      .setTitle(`${user.username}#${user.discriminator}`)
+      .setColor(0x00AE86)
+      .setDescription(message.content);
+    channel.send({embed});
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+
+client.on('ready', () => {
+  console.log('I am ready!');
+  officerChannel = client.channels.get(officerChannelId);
+  officerForForwarding = client.guilds.get(guildId).members.get(officerForForwardingId).user;
+});
+
+client.on('message', message => {
+  if (!message.channel) return;
+  if (message.channel.id === applicationChannelId) pasteApplicationToOfficerChannel(message);
+  if (message.channel.type === 'dm') relayDMsToOfficer(message);
 });
 
 client.login(clientBotId);
